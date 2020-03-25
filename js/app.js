5514c8167ff691a4cdf2f5429bc5fd6ca190a03c
indykk90 <k.indelak@gmail.com>
2020-03-25 20:24:01 +0100

    coronavirus map

---
+++ js/app.js
@@ -0,0 +1,9 @@
+import { $, $$, to24HourFormat, formatRangeLabel, toDateInputFormat } from './helpers.js';
+import { center, hereCredentials } from './config.js';
+
+//Height calculations
+const height = $('#content-group-1').clientHeight || $('#content-group-1').offsetHeight;
+$('.content').style.height = height + 'px';
+
+https://2.base.maps.ls.hereapi.com/maptile/2.1/basetile/newest/normal.day/14/4769/6203/256/png8
+?apiKey={YzIYwQpS4P_M_ufPCTR2QGt7jkBQd1NmAYUnB7eGrDQ}
---
+++ js/config.js
@@ -0,0 +1,13 @@
+const center = { 
+   lat: 52.5159, 
+   lng: 13.3777, 
+   text: 'Berlin, Germany'
+}
+
+const hereCredentials = {
+   id: 'YOUR-HERE-ID',
+   code: 'YOUR-HERE-CODE',
+   apikey: 'YzIYwQpS4P_M_ufPCTR2QGt7jkBQd1NmAYUnB7eGrDQ'
+}
+
+export { center, hereCredentials };
